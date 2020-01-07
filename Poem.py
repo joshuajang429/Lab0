20515ded48fa53cd44b7c20580229a19f687dd04
joshuajang429 <joshua.jang429@gmail.com>
2020-01-07 12:24:34 -0800

    Initial commit

---
+++ Poem.py
@@ -0,0 +1,6 @@
+print('Roses are red\n'
+      'Violets are blue\n'
+      'Sugar is sweet\n'
+      'But I have commitment issues\n'
+      'So I\'d rather just be friends\n'
+      'At this point in our relationship.')
---
+++ Temperature.py
@@ -0,0 +1,4 @@
+celsius = 10
+faren = float(celsius*(9/5)+32)
+
+print(faren)
