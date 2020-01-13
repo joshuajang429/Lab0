fa3f254f22b08bd68d41b7f99e5ae31761fa94f6
joshuajang429 <joshua.jang429@gmail.com>
2020-01-13 10:53:16 -0800

    Lab00

--- Poem.py
+++
@@ -1,6 +0,0 @@
-print('Roses are red\n'
-      'Violets are blue\n'
-      'Sugar is sweet\n'
-      'But I have commitment issues\n'
-      'So I\'d rather just be friends\n'
-      'At this point in our relationship.')
--- Temperature.py
+++
@@ -1,4 +0,0 @@
-celsius = 10
-faren = float(celsius*(9/5)+32)
-
-print(faren)
